edb5ceff7d8810cabdaf5543764449d6c590f277
KimJin0315 <xnxl159@naver.com>
2025-06-16 00:30:46 +0900

    Playfab SDK 설치

---
+++ Photon Network Game/Assets/Scripts/PlyafabManager.cs
@@ -0,0 +1,46 @@
+using PlayFab;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using PlayFab.ClientModels;
+using System;
+
+public class PlyafabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] string version;
+
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = version;
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Access()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+
+    void Failure(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+
+}
