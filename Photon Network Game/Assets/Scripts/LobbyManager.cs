13d72305b953126cc10caa215d0b7cd35921a4f2
KimJin0315 <xnxl159@naver.com>
2025-06-17 19:44:29 +0900

    (2025.06.17) 룸 생성 시스템

--- Photon Network Game/Assets/Scripts/LobbyManager.cs
+++ Photon Network Game/Assets/Scripts/LobbyManager.cs
@@ -7,7 +7,53 @@ using Photon.Realtime;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] Transform parentTransform;
+
+    [SerializeField] Dictionary<string, GameObject> dictionary = new Dictionary<string, GameObject>();
 
 
+
+    public void OnCreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+
+        roomOptions.MaxPlayers = 4;
+
+        roomOptions.IsOpen = true;
+
+        roomOptions.IsVisible = true;
+
+        PhotonNetwork.CreateRoom("Battle", roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        GameObject prefab = null;
+
+        foreach (RoomInfo room in roomList)
+        {
+            // 룸이 삭제된 경우
+            if (room.RemovedFromList == true)
+            {
+                dictionary.TryGetValue(room.Name, out prefab);
+
+                Destroy(prefab);
+
+                dictionary.Remove(room.Name);
+            }
+            // 룸의 정보가 변경되는 경우
+            else
+            {
+
+            }
+            
+
+        }
+    }
 
 }
