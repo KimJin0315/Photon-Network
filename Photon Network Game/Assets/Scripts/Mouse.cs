b939c0953cb8fabd51209a507daeb682ce697147
KimJin0315 <xnxl159@naver.com>
2025-06-23 19:44:39 +0900

    (2025.06.23) 마우스 시스템

---
+++ Photon Network Game/Assets/Scripts/Mouse.cs
@@ -0,0 +1,27 @@
+using System;
+using Photon.Pun;
+using UnityEngine;
+
+public class Mouse : MonoBehaviourPun
+{
+    void Start()
+    {
+        SetMouse(false);
+    }
+
+    public void SetMouse(bool state)
+    {
+        if (photonView.IsMine)
+        {
+            Cursor.visible = state;
+
+            Cursor.lockState = (CursorLockMode)Convert.ToInt32(!state);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SetMouse(true);
+    }
+
+}
